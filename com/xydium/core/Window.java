dcdb95ed988f13ec5aee1b7d44614b0a489d73fe
Tim Hornick (AKA Xydium) <prodriver54@gmail.com>
2016-06-12 21:42:15 -0400

    Patched Misaligned Rendering

--- com/xydium/core/Window.java
+++ com/xydium/core/Window.java
@@ -1,10 +1,12 @@
 package com.xydium.core;
 
+import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import com.xydium.geometry.Vec2i;
 import com.xydium.input.Input;
@@ -17,7 +19,7 @@ public class Window {
 	private String title;
 	
 	private JFrame frame;
-
+	
 	public Window(int width, int height, double scale, String title) {
 		this.width = width;
 		this.height = height;
@@ -28,7 +30,7 @@ public class Window {
 	}
 	
 	public void drawFrame(Image image) {
-		frame.getGraphics().drawImage(image, 0, 0, frame.getWidth(), frame.getHeight(), null);
+		frame.getContentPane().getGraphics().drawImage(image, 0, 0, (int) (width * scale), (int) (height * scale), null);
 	}
 	
 	public void destroy() {
@@ -53,7 +55,11 @@ public class Window {
 	
 	public void rescale(double scale) {
 		this.scale = scale;
-		frame.setSize((int) (width * scale), (int) (height * scale));
+		frame.getContentPane().setMinimumSize(new Dimension((int)(width * scale), (int)(height * scale)));
+		frame.getContentPane().setPreferredSize(new Dimension((int)(width * scale), (int)(height * scale)));
+		frame.getContentPane().setMaximumSize(new Dimension((int)(width * scale), (int)(height * scale)));
+		frame.pack();
+		frame.setSize(frame.getWidth() - 10, frame.getHeight() - 10);
 	}
 	
 	public Vec2i getDimensions() {
@@ -61,7 +67,6 @@ public class Window {
 	}
 	
 	private void configureFrame() {
-		rescale(scale);
 		frame.addWindowListener(new WindowListener() {
 			public void windowActivated(WindowEvent arg0) {}
 			public void windowClosed(WindowEvent arg0) {}
@@ -72,7 +77,10 @@ public class Window {
 			public void windowOpened(WindowEvent arg0) {}
 		});
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setContentPane(new JPanel());
+		rescale(scale);
 		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 		frame.addKeyListener(Input.init());
 	}
