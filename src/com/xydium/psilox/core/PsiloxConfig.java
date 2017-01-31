89142863623e7c8ad78de13071fb461c6df66779
Tim Hornick (AKA Xydium) <prodriver54@gmail.com>
2017-01-31 09:22:31 -0500

    Log Config

--- src/com/xydium/psilox/core/PsiloxConfig.java
+++ src/com/xydium/psilox/core/PsiloxConfig.java
@@ -1,6 +1,7 @@
 package com.xydium.psilox.core;
 
 import com.xydium.psilox.rendering.Color;
+import com.xydium.psilox.utilities.Log;
 
 public class PsiloxConfig {
 
@@ -12,5 +13,22 @@ public class PsiloxConfig {
 	public boolean fullscreen = false, clearscreen = true, doubleBuffer = true, console = true;
 	public String title = "Psilox", logLevel = "DEBUG";
 	public Color clearColor = new Color(0.0f, 0.0f, 0.0f);
+	
+	public void logConfig(Psilox psilox) {
+		Log.debug("###### Psilox Started (Runtime %d), Reviewing Configuration", psilox.id());
+		Log.debug("## Width: %d", width);
+		Log.debug("## Height: %d", height);
+		Log.debug("## Title: %s", title);
+		Log.debug("## Update Rate: %d/sec", ups);
+		Log.debug("## Frame Rate: %d/sec", fps);
+		Log.debug("## Scale: %.2f", scale);
+		Log.debug("## Fullscreen: %b", fullscreen);
+		Log.debug("## Clearscreen: %b", clearscreen);
+		Log.debug("## Clearcolor: %.2f, %.2f, %.2f, %.2f", clearColor.r, clearColor.g, clearColor.b, clearColor.a);
+		Log.debug("## Doublebuffering: %b", doubleBuffer);
+		Log.debug("## Using System Printstream: %b", console);
+		Log.debug("## Log Level of Detail: %s", logLevel);
+		Log.debug("######");
+	}
 	
 }
