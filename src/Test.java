a0213dd63fddc629d9326c3d63868db9596498bb
Xydium <prodriver54@gmail.com>
2017-05-23 19:29:36 -0400

    Included Last 2 Files

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,49 @@
 import psilox.core.Psilox;
 import psilox.core.Window;
 import psilox.graphics.Color;
+import psilox.graphics.Texture;
+import psilox.math.Anchor;
+import psilox.math.Rect;
+import psilox.math.Vec;
+import psilox.node.Node;
+import psilox.utility.Time;
 
-public class Test {
+public class Test extends Node {
+
+	static final int WIDTH = 500;
+	static final int HEIGHT = 500;
+	
+	public Test(String tag) {
+		super(tag);
+	}
+	
+	public void enteredTree() {
+		if(getParent().RUID == Psilox.root.RUID) {
+			addChild(new Test("test2"));
+			position = new Vec(WIDTH / 2, HEIGHT / 2);
+		} else {
+			position = new Vec(50, 0);
+		}
+		dimensions = new Vec(50);
+		anchor = Anchor.MIDDLE_RIGHT;
+		texture = new Texture(3, 3);
+		texture.setData(new int[]{0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFF00, 0xFFFF00FF, 0xFF00FFFF,
+					0xFFFFFFFF, 0xFF8E8E8E, 0xFF252525}, 3, 3);
+		textureRegion = new Rect(.5f, .5f, 2f, 2f);
+	}
+	
+	public void update() {
+		rotation += 2;
+		
+		if(tag.equals("test3")) {
+			position.x = (WIDTH - 200) * (float) Time.flipFlop(0, Time.SECOND * 13) + 100;
+			position.y = (HEIGHT - 200) * (float) Time.flipFlop(0, Time.SECOND * 5) + 100;
+			scale = new Vec(2).scl((float) Time.flipFlop(0, Time.SECOND * 7));
+		}		
+	}
 
 	public static void main(String[] args) {
-		Psilox.start(new Window("Test", 1280, 720, false, false, Color.BLACK));
+		Psilox.start(new Window("Test", WIDTH, HEIGHT, false, false, Color.BLACK), new Test("test"));
 	}
 	
 }
--- src/psilox/core/Window.java
+++ src/psilox/core/Window.java
@@ -1,49 +1,13 @@
 package psilox.core;
 
-import static org.lwjgl.glfw.GLFW.GLFW_CONTEXT_VERSION_MAJOR;
-import static org.lwjgl.glfw.GLFW.GLFW_CONTEXT_VERSION_MINOR;
-import static org.lwjgl.glfw.GLFW.GLFW_DECORATED;
-import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_CORE_PROFILE;
-import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_FORWARD_COMPAT;
-import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_PROFILE;
-import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
-import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
-import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
-import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
-import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
-import static org.lwjgl.glfw.GLFW.glfwInit;
-import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
-import static org.lwjgl.glfw.GLFW.glfwPollEvents;
-import static org.lwjgl.glfw.GLFW.glfwSetWindowMonitor;
-import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
-import static org.lwjgl.glfw.GLFW.glfwShowWindow;
-import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
-import static org.lwjgl.glfw.GLFW.glfwTerminate;
-import static org.lwjgl.glfw.GLFW.glfwWindowHint;
-import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
-import static org.lwjgl.opengl.GL11.GL_BLEND;
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
-import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
-import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
-import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
-import static org.lwjgl.opengl.GL11.GL_TRUE;
-import static org.lwjgl.opengl.GL11.GL_VERSION;
-import static org.lwjgl.opengl.GL11.glBlendFunc;
-import static org.lwjgl.opengl.GL11.glClear;
-import static org.lwjgl.opengl.GL11.glClearColor;
-import static org.lwjgl.opengl.GL11.glDrawArrays;
-import static org.lwjgl.opengl.GL11.glEnable;
-import static org.lwjgl.opengl.GL11.glGetError;
-import static org.lwjgl.opengl.GL11.glGetString;
-import static org.lwjgl.opengl.GL11.glViewport;
-import static org.lwjgl.opengl.GL13.GL_TEXTURE0;
-import static org.lwjgl.opengl.GL13.glActiveTexture;
-import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
-import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
-import static org.lwjgl.opengl.GL30.glBindVertexArray;
-import static org.lwjgl.system.MemoryUtil.NULL;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL13.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+import java.util.Stack;
 
 import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
@@ -53,7 +17,9 @@ import psilox.graphics.Mesh;
 import psilox.graphics.Shader;
 import psilox.graphics.Texture;
 import psilox.math.Mat4;
+import psilox.math.Rect;
 import psilox.math.Vec;
+import psilox.node.Node;
 import psilox.utility.Log;
 
 public class Window {
@@ -65,6 +31,11 @@ public class Window {
 	private boolean undecorated;
 	private Color clearColor;
 	private long handle;
+	
+	private Mesh defaultMesh;
+	private Shader defaultShader;
+	private Mat4 projection;
+	private Stack<Mat4> transforms;
 	
 	public Window() {
 		this("Psilox", 500, 500, false, false, Color.BLACK);
@@ -111,9 +82,7 @@ public class Window {
 		GL.createCapabilities();
 		
 		glEnable(GL_DEPTH_TEST);
-		//glEnable(GL_TEXTURE_2D);
 		glEnable(GL_BLEND);
-		
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		
 		glClearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
@@ -125,6 +94,11 @@ public class Window {
 		}
 		
 		Log.internal("Created Window using GL: %s", getGLVersion());
+		
+		defaultMesh = Mesh.unitSquare();
+		defaultShader = new Shader("shaders/psilox.shd");
+		projection = Mat4.orthographic(0, width, 0, height, 10, -10);
+		transforms = new Stack<Mat4>();
 	}
 	
 	void pollEvents() {
@@ -159,56 +133,63 @@ public class Window {
 		return glfwWindowShouldClose(handle);
 	}
 	
-	private Mesh debugMesh;
-	private Shader debugShader;
-	private Texture tex;
-	void debugRender() {
-		if(debugMesh == null) {
-			float[] verts = {
-				0, 1, 0f, //Top Left
-				0, 0, 0f, //Bottom Left
-				1, 0, 0f, //Bottom Right
-				1, 0, 0f, //Bottom Right 
-				1, 1, 0f, //Top Right
-				0, 1, 0f //Top Left
-			};
-			
-			float[] tcoords = {
-				0f, 0f,
-				0f, 1f,
-				1f, 1f,
-				1f, 1f,
-				1f, 0f,
-				0f, 0f
-			};
-			
-			debugMesh = Mesh.loadToVao(verts, tcoords);
-			debugShader = new Shader("shaders/psilox.shd");
-			tex = new Texture(3, 2);
-			tex.setData(new int[]{0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFF00, 0xFFFF00FF, 0xFF00FFFF,
-					0xFFFFFFFF, 0xFF8E8E8E, 0xFF252525}, 3, 3);
-		};
-		
-		debugShader.enable();
-		glBindVertexArray(debugMesh.getVaoID());
+	void renderTree(Node root) {
+		glBindVertexArray(defaultMesh.getVaoID());
 		glEnableVertexAttribArray(0);
 		glEnableVertexAttribArray(1);
 		glActiveTexture(GL_TEXTURE0);
-		tex.bind();
-		debugShader.setUniformMat4f("u_projection", Mat4.orthographic(0, width, 0, height, 10, -10));
-		debugShader.setUniformMat4f("u_transform", Mat4.transform(new Vec(width / 2, height / 2), Psilox.ticks, new Vec(90)));
-		debugShader.setUniform2f("u_dimensions", 50, 50);
-		debugShader.setUniform2f("u_tex_dimensions", 3, 3);
-		debugShader.setUniform2f("u_tex_botleft", .5f, .5f);
-		debugShader.setUniform2f("u_tex_topright", 2.5f, 2.5f);
-		debugShader.setUniform2f("u_anchor", .5f, .5f);
-		debugShader.setUniform4f("u_modulate", Color.WHITE);
-		glDrawArrays(GL_TRIANGLES, 0, debugMesh.getVertCount());
-		tex.unbind();
+		
+		defaultShader.enable();
+		defaultShader.setUniformMat4f("u_projection", projection);
+		
+		renderNode(root);
+		
+		defaultShader.disable();
+		
 		glDisableVertexAttribArray(0);
 		glEnableVertexAttribArray(1);
 		glBindVertexArray(0);
-		debugShader.disable();
+	}
+	
+	private void renderNode(Node node) {
+		if(transforms.isEmpty()) {
+			transforms.push(Mat4.transform(node.position, node.rotation, node.scale));
+		} else {
+			transforms.push(Mat4.transform(transforms.peek(), node.position, node.rotation, node.scale));
+		}
+	
+		for(Node n : node.getChildrenUnsafe()) {
+			renderNode(n);
+		}
+		
+		defaultShader.setUniformMat4f("u_transform", transforms.peek());
+		defaultShader.setUniform2f("u_dimensions", node.dimensions.x, node.dimensions.y);
+		
+		if(node.texture != null) {
+			node.texture.bind();
+			defaultShader.setUniform1i("u_tex_valid", 1);
+			final int width = node.texture.getWidth();
+			final int height = node.texture.getHeight();
+			defaultShader.setUniform2f("u_tex_dimensions", width, height);
+			if(node.textureRegion != null) {
+				defaultShader.setUniform4f("u_tex_region", node.textureRegion);
+			} else {
+				defaultShader.setUniform4f("u_tex_region", new Rect(0, 0, width, height));
+			}
+		} else {
+			defaultShader.setUniform1i("u_tex_valid", 0);
+		}
+				
+		defaultShader.setUniform2f("u_anchor", node.anchor.x, node.anchor.y);
+		defaultShader.setUniform4f("u_modulate", node.modulate);
+		
+		glDrawArrays(GL_TRIANGLES, 0, defaultMesh.getVertCount());
+		
+		if(node.texture != null) {
+			node.texture.unbind();
+		}
+		
+		transforms.pop();
 	}
 	
 }
