db7c71a53914b649c37f5d2b860416467b47246b
Xydium <prodriver54@gmail.com>
2017-05-23 21:44:13 -0400

    Slight Test Tweaks

--- src/Test.java
+++ src/Test.java
@@ -10,36 +10,34 @@ import psilox.utility.Time;
 
 public class Test extends Node {
 
-	static final int WIDTH = 500;
-	static final int HEIGHT = 500;
+	static final int WIDTH = 1280;
+	static final int HEIGHT = 720;
 	
 	public Test(String tag) {
 		super(tag);
 	}
 	
 	public void enteredTree() {
-		if(getParent().RUID == Psilox.root.RUID) {
-			addChild(new Test("test2"));
-			position = new Vec(WIDTH / 2, HEIGHT / 2);
-		} else {
-			position = new Vec(50, 0);
+		if(RUID < 20) {
+			addChild(new Test("test"));
+			position = new Vec(15, 0, .1f);
 		}
-		dimensions = new Vec(50);
-		anchor = Anchor.MIDDLE_RIGHT;
+		if(RUID == 0) {
+			position = new Vec(WIDTH / 2, HEIGHT / 2);
+			scale = new Vec(3);
+		} else {
+			scale = new Vec(0.99f);
+		}
+		dimensions = new Vec(10);
+		anchor = Anchor.MIDDLE;
 		texture = new Texture(3, 3);
-		texture.setData(new int[]{0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFF00, 0xFFFF00FF, 0xFF00FFFF,
-					0xFFFFFFFF, 0xFF8E8E8E, 0xFF252525}, 3, 3);
-		textureRegion = new Rect(.5f, .5f, 2f, 2f);
+		texture.setData(new int[]{0x8EFF0000, 0x8E00FF00, 0x8E0000FF, 0x8EFFFF00, 0x8EFF00FF, 0x8E00FFFF,
+					0x8EFFFFFF, 0x8E8E8E8E, 0x8E252525}, 3, 3);
+		textureRegion = new Rect(0, 0, 3, 3);
 	}
 	
 	public void update() {
-		rotation += 2;
-		
-		if(tag.equals("test3")) {
-			position.x = (WIDTH - 200) * (float) Time.flipFlop(0, Time.SECOND * 13) + 100;
-			position.y = (HEIGHT - 200) * (float) Time.flipFlop(0, Time.SECOND * 5) + 100;
-			scale = new Vec(2).scl((float) Time.flipFlop(0, Time.SECOND * 7));
-		}		
+		rotation += 1;
 	}
 
 	public static void main(String[] args) {
